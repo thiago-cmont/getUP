84311536c02bff86689f9c6f9fc37aa6e3151f3c
Thiago Correia <thiagocorreiamonteiro@gmail.com>
2021-03-14 09:10:40 -0300

    Init develop

--- src/components/ChallangeBox.tsx
+++ src/components/ChallangeBox.tsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { ChallangesContext } from "../context/ChallangesContext";
 import { CountdownContext } from "../context/CountdownContext";
 import styles from "../styles/components/ChallangeBox.module.css";
-
+//'teste'
 export function ChallangeBox() {
   const { activeChallange, resetChallange, completeChallange } = useContext(
     ChallangesContext
